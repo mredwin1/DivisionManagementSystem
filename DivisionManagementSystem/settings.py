21873354dfe755e1c2dc8df5e7539a4a26779d8b
Edwin Cruz <ecruz1113@gmail.com>
2020-11-09 13:20:25 -0500

    Add MySql as the database
    Did this so there is not a different database in production and in
    development.

--- DivisionManagementSystem/settings.py
+++ DivisionManagementSystem/settings.py
@@ -87,10 +87,14 @@ WSGI_APPLICATION = 'DivisionManagementSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dms-db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'PORT': 3306,
     }
 }
 
