cb11ff7b89c4700b4a3413d88517049993aed5ff
Edwin Cruz <ecruz1113@gmail.com>
2020-11-10 22:05:09 -0500

    Add tests for export_phone_list view

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -44,3 +44,44 @@ class TestEmployeeInfoView(TestCase):
         self.client.logout()
 
         self.assertEqual(resp.status_code, 200)
+
+
+class TestExportPhoneList(TestCase):
+    def setUp(self):
+        """Create an authorized Django user for testing purposes"""
+        self.user = Employee.objects.create_user(
+            username='test.user',
+            password='test',
+            first_name='Test',
+            last_name='User',
+            employee_id=000000
+        )
+
+    def test_redirect_when_logged_out(self):
+        """Test that the user is redirected to the log in page when not logged in"""
+        url = reverse('main-export-phone-list')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+
+    def test_get_when_logged_in_no_permissions(self):
+        """Test that the view returns a HTTP 403 when the user is logged in but does not have can_export_phone_list
+         permissions"""
+        url = reverse('main-export-phone-list')
+        self.client.force_login(self.user)
+        resp = self.client.get(url)
+        self.client.logout()
+
+        self.assertEqual(resp.status_code, 403)
+
+    def test_get_when_logged_in_with_permissions(self):
+        """Test that the view returns a HTTP 200 and that content-disposition is correct when the user is logged in and
+         has can_export_phone_list permissions"""
+        url = reverse('main-export-phone-list')
+        permission = Permission.objects.get(codename='can_export_phone_list')
+
+        self.user.user_permissions.add(permission)
+        self.client.force_login(self.user)
+        resp = self.client.get(url)
+        self.client.logout()
+
+        self.assertEqual(resp.get('content-disposition'), 'attachment;filename="Division 12 - Phone List.pdf"')
