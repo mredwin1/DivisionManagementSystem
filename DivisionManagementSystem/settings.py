2c39853ff847e03625836907ff235817cde6a15c
edwin.cruz <ecruz1113@gmail.com>
2020-11-12 16:05:59 -0500

    Configure loggers and handlers for django logging.
    This will allow to output logs for when everything is starting up so it
    is easier to see that everything is happening.

--- DivisionManagementSystem/settings.py
+++ DivisionManagementSystem/settings.py
@@ -84,6 +84,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DivisionManagementSystem.wsgi.application'
 
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
