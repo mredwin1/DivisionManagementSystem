806b315235f8c6a43a0f8bdc7a33f97773211831
Edwin Cruz <55803329+mredwin1@users.noreply.github.com>
2020-12-02 13:19:43 -0500

    Update EMAIL_PORT to be int
    Django expects this to be an int and reading from the environment variable gets passed as a str which would cause errors.

--- DivisionManagementSystem/settings.py
+++ DivisionManagementSystem/settings.py
@@ -179,7 +179,7 @@ DJANGO_NOTIFICATIONS_CONFIG = {'USE_JSONFIELD': True}
 # Django Emailing
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
-EMAIL_PORT = os.environ.get('EMAIL_PORT', '')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', ''))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = bool(int(os.environ.get('EMAIL_USE_TLS', 1)))
