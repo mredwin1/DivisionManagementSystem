a1cdf836712886519fa789359c04330a5297b8ec
Edwin Cruz <55803329+mredwin1@users.noreply.github.com>
2020-12-02 13:16:38 -0500

    Edit email settings to use environment variables
    This will allow for each individual to use their own smtp server as opposed to being locked down to having to use outlook like before.

--- DivisionManagementSystem/settings.py
+++ DivisionManagementSystem/settings.py
@@ -178,12 +178,12 @@ DJANGO_NOTIFICATIONS_CONFIG = {'USE_JSONFIELD': True}
 
 # Django Emailing
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', '')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_USE_TLS = bool(int(os.environ.get('EMAIL_USE_TLS', 1)))
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'notify@dms.com')
 
 # Domain and Protocol
 BASE_URL = 'http://10.100.12.67:8000'
