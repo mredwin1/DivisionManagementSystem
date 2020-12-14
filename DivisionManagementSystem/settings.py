7b6e583b03165a261db58da8bb1777bd03fa4b1f
Edwin Cruz <ecruz1113@gmail.com>
2020-12-14 10:24:10 -0500

    Remove STATICFILES_DIRS, Change static and media
    STATICFILES_DIRS is just to add additional static file locations to
    django which I do not have and do not need. Also, just changed the names
    to of static and media root to be consistent with what I had before.

--- DivisionManagementSystem/settings.py
+++ DivisionManagementSystem/settings.py
@@ -163,13 +163,11 @@ if USE_S3:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 else:
-    STATIC_URL = '/staticfiles/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-MEDIA_URL = '/mediafiles/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Custom User & Log In
 AUTH_USER_MODEL = 'employees.Employee'
