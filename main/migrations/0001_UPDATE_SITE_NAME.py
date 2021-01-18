dba155cebf160aab0ff609ebc3d930e0880eaffb
Edwin Cruz <ecruz1113@gmail.com>
2021-01-18 10:59:31 -0500

    Set default for domain env variable

--- main/migrations/0001_UPDATE_SITE_NAME.py
+++ main/migrations/0001_UPDATE_SITE_NAME.py
@@ -8,7 +8,7 @@ from django.conf import settings
 
 def update_site_name(apps, schema_editor):
     SiteModel = apps.get_model('sites', 'Site')
-    domain = os.environ['DOMAIN']
+    domain = os.environ.get('DOMAIN', 'dms.localhost')
 
     SiteModel.objects.update_or_create(
         pk=settings.SITE_ID,
