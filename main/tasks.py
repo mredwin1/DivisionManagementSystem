aadb7b40cd28ba058820de380e7610a2f269d2a8
Edwin Cruz <ecruz1113@gmail.com>
2021-02-15 13:53:47 -0500

    Remove try catch for testing

--- main/tasks.py
+++ main/tasks.py
@@ -28,52 +28,52 @@ def import_drivers(path):
                 sheet = wb['data']
                 counter = 0
                 for row in sheet.iter_rows(min_row=2):
+                    # try:
+                    last_name = f'{row[0].value.lower()[0].upper()}{row[0].value.lower()[1:]}'
+                    first_name = f'{row[1].value.lower()[0].upper()}{row[1].value.lower()[1:]}'
+                    employee_id = row[2].value
+                    position = row[3].value.lower()
+                    hire_date = datetime.datetime.strptime(str(row[4].value), '%Y%m%d')
+                    application_date = datetime.datetime.strptime(str(row[5].value), '%Y%m%d')
+                    classroom_date = datetime.datetime.strptime(str(row[6].value), '%Y%m%d')
+                    company_name = row[7].value
+                    is_part_time = True if row[8].value == 'TRUE' else False
+                    primary_phone = row[9].value
+                    secondary_phone = row[10].value
+                    ss_number = row[11].value
+
+                    company = Company.objects.get(display_name=company_name)
+
+                    new_employee = Employee(
+                        first_name=first_name,
+                        last_name=last_name,
+                        employee_id=employee_id,
+                        primary_phone=primary_phone,
+                        secondary_phone=secondary_phone,
+                        hire_date=hire_date,
+                        application_date=application_date,
+                        classroom_date=classroom_date,
+                        company=company,
+                        is_part_time=is_part_time,
+                        username=employee_id,
+                        position=position,
+                        is_active=True,
+                    )
+
+                    password = f'{company}{first_name[0].upper()}{last_name[0].upper()}{ss_number}'
+
+                    new_employee.set_password(password)
+
                     try:
-                        last_name = f'{row[0].value.lower()[0].upper()}{row[0].value.lower()[1:]}'
-                        first_name = f'{row[1].value.lower()[0].upper()}{row[1].value.lower()[1:]}'
-                        employee_id = row[2].value
-                        position = row[3].value.lower()
-                        hire_date = datetime.datetime.strptime(str(row[4].value), '%Y%m%d')
-                        application_date = datetime.datetime.strptime(str(row[5].value), '%Y%m%d')
-                        classroom_date = datetime.datetime.strptime(str(row[6].value), '%Y%m%d')
-                        company_name = row[7].value
-                        is_part_time = True if row[8].value == 'TRUE' else False
-                        primary_phone = row[9].value
-                        secondary_phone = row[10].value
-                        ss_number = row[11].value
-
-                        company = Company.objects.get(display_name=company_name)
-
-                        new_employee = Employee(
-                            first_name=first_name,
-                            last_name=last_name,
-                            employee_id=employee_id,
-                            primary_phone=primary_phone,
-                            secondary_phone=secondary_phone,
-                            hire_date=hire_date,
-                            application_date=application_date,
-                            classroom_date=classroom_date,
-                            company=company,
-                            is_part_time=is_part_time,
-                            username=employee_id,
-                            position=position,
-                            is_active=True,
-                        )
-
-                        password = f'{company}{first_name[0].upper()}{last_name[0].upper()}{ss_number}'
-
-                        new_employee.set_password(password)
-
-                        try:
-                            profile_picture = BytesIO(zipfile.read(f'drivers/pictures/{employee_id}.jpg'))
-                            new_employee.profile_picture.save(f'{employee_id}.jpg', profile_picture)
-                        except KeyError:
-                            pass
-
-                        new_employee.save()
-                        counter += 1
-                    except:
+                        profile_picture = BytesIO(zipfile.read(f'drivers/pictures/{employee_id}.jpg'))
+                        new_employee.profile_picture.save(f'{employee_id}.jpg', profile_picture)
+                    except KeyError:
                         pass
+
+                    new_employee.save()
+                    counter += 1
+                    # except:
+                    #     pass
             except KeyError:
                 pass
         except KeyError:
