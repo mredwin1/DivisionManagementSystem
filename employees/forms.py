b2cfc15b83451e279842a1b52afcef9c389c111e
Edwin Cruz <ecruz1113@gmail.com>
2021-05-26 17:41:04 -0400

    Replace day 3 attendance notification with day 14

--- employees/forms.py
+++ employees/forms.py
@@ -569,7 +569,7 @@ class NotificationSettings(forms.ModelForm):
         model = Employee
         fields = ['email_7attendance', 'email_10attendance', 'email_written', 'email_last_final', 'email_removal',
                   'email_safety_point', 'email_termination', 'email_add_hold', 'email_rem_hold', 'email_add_settlement',
-                  'email_new_time_off', 'email_new_employee', 'email_attendance_doc_day3', 'email_attendance_doc_day5',
+                  'email_new_time_off', 'email_new_employee', 'email_attendance_doc_day14', 'email_attendance_doc_day5',
                   'email_attendance_doc_day7', 'email_attendance_doc_day10', 'email_safety_doc_day3',
                   'email_safety_doc_day5', 'email_safety_doc_day7', 'email_safety_doc_day10',
                   'email_counseling_doc_day3', 'email_counseling_doc_day5', 'email_counseling_doc_day7',
