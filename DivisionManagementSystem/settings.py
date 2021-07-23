ecc5f3032e0ae50d7a5becf695c74901a6001391
Edwin Cruz <ecruz1113@gmail.com>
2021-07-23 13:24:04 -0400

    Change default ACL to private

--- DivisionManagementSystem/settings.py
+++ DivisionManagementSystem/settings.py
@@ -157,7 +157,7 @@ if USE_S3:
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = None
+    AWS_DEFAULT_ACL = 'private'
     AWS_S3_CUSTOM_DOMAIN = f'd13t82vqi828w2.cloudfront.net'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # S3 static settings
