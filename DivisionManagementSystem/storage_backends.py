b9db9c96bd9d36c5b89309975b2f0530e85de232
Edwin Cruz <ecruz1113@gmail.com>
2021-07-23 13:25:17 -0400

    Remove default acl for storage classes

--- DivisionManagementSystem/storage_backends.py
+++ DivisionManagementSystem/storage_backends.py
@@ -4,10 +4,10 @@ from django.conf import settings
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
-    default_acl = 'public-read'
+    # default_acl = 'public-read'
 
 
 class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
-    default_acl = 'public-read'
+    # default_acl = 'public-read'
     file_overwrite = False
